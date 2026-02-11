ec6b33a48702c07d5c78e4580da1909fd97fd042
Caden Myers <cjm2304@columbia.edu>
2026-02-11 11:16:50 -0500

    update copyright year in version.py

--- src/scikit_package/version.py
+++ src/scikit_package/version.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 ##############################################################################
 #
-# (c) 2024 The Trustees of Columbia University in the City of New York.
+# (c) 2026 The Trustees of Columbia University in the City of New York.
 # All rights reserved.
 #
 # File coded by: Billinge Group members and community contributors.
@@ -24,5 +24,3 @@ try:
     __version__ = version("scikit-package")
 except PackageNotFoundError:
     __version__ = "unknown"
-
-# End of file
